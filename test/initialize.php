4bcc3078edf3d4531a70439b24b444bfc21008cf
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-11-06 09:13:35 +0100

    Testing

---
+++ test/initialize.php
@@ -0,0 +1,3 @@
+<?php
+require("../src/full_text_search_query.php");
+require("../src/full_text_search_query_like.php");
---
+++ test/tc_full_text_search_query.php
@@ -0,0 +1,77 @@
+<?php
+class TcFullTextSearchQuery extends TcBase {
+
+	function test(){
+		$ftsq = new FullTextSearchQuery();
+
+		$this->_testValidParse("beer",array(
+			array(
+				"term" => "beer",
+				"type" => "term",
+				"char_position" => 0,
+				"occurrence" => "MUST",
+				"childs" => array (),
+			)
+		));
+
+		$this->_testValidParse("not beer",array(
+			array (
+				"term" => "beer",
+				"type" => "term",
+				"char_position" => 4,
+				"occurrence" => "NOT",
+				"childs" => array (),
+			),
+		));
+
+		// "beer wine" is same like "beer and wine"
+
+		$beer_and_wine = array(
+			array (
+				"term" => "beer",
+				"type" => "term",
+				"char_position" => 0,
+				"occurrence" => "MUST",
+				"childs" => array (),
+			),
+			array (
+				"term" => "wine",
+				"type" => "term",
+				"char_position" => 5,
+				"occurrence" => "MUST",
+				"childs" => array (),
+			),
+		);
+		$this->_testValidParse("beer wine",$beer_and_wine);
+
+		$beer_and_wine[1]["char_position"] = 9;
+		$this->_testValidParse("beer and wine",$beer_and_wine);
+
+		// "beer not wine"
+
+		$beer_not_wine = array(
+			array (
+				"term" => "beer",
+				"type" => "term",
+				"char_position" => 0,
+				"occurrence" => "MUST",
+				"childs" => array (),
+			),
+			array (
+				"term" => "wine",
+				"type" => "term",
+				"char_position" => 9,
+				"occurrence" => "NOT",
+				"childs" => array (),
+			),
+		);
+		$this->_testValidParse("beer not wine",$beer_not_wine);
+	}
+
+	function _testValidParse($query,$expected_tree){
+		$ftsq = new FullTextSearchQuery();
+
+		$this->assertEquals(true,$ftsq->parse($query),"A valid query expected: $query");
+		$this->assertEquals($expected_tree,$ftsq->get_tree());
+	}
+}
